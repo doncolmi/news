414c99c08cf1cabcb08a96a2ff41a95a778af421
doncolmi <matathresh1740@naver.com>
2020-03-14 23:02:15 +0900

    add

--- services/test.js
+++ services/test.js
@@ -1,41 +1,24 @@
 const cron = require('node-cron');
 
-const ax = require('axios');
+const request = require('request');
 const cheerio = require('cheerio');
-const san = require('sanitize-html');
+const iconv = require('iconv-lite');
 
-const getTitle = async (url) => {
-    try{
-        return await ax.get(url);
-    } catch(error) {
-        console.log(error);
-    }
-};
 
 cron.schedule('*/10 * * * * *', () => {
     console.log("이거맞아 대성아");
     const url = `https://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=105`;
 
-    getTitle(url)
-        .then(html => {
-            console.log(html.head);
-            let ulList = [];
-            const $ = cheerio.load(html.data);
-
-            const $bodyList = san($("ul.type06_headline").children("li"), {
-                parser: {
-                    decodeEntities: true
-                }
-            });
-console.log($bodyList);
-            // $bodyList.each(function(i, elem) {
-            //     const content = $(this).find('dl dt:nth-child(2)').text().trim();
-            //     ulList[i] = {
-            //         title : content
-            //     }
-            // });
-            // console.log(ulList);
-        })
+    request({url : url, encoding : null}, function (error, response, body) {
+        let htmlDoc = iconv.decode(body, 'MS949');
+        let result = [];
+        const $ = cheerio.load(htmlDoc);
+        let col = $("ul.type06_headline").children("li");
+        col.each(function(i, elem) {
+            result[i] = $(this).find('dl dt:nth-child(2)').text().trim();
+        });
+        console.log(result);
+    });
 })
 
 module.exports = cron;
