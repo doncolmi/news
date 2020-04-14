3c1588441d41de33a64acffb945536cab77167f2
doncolmi <matathresh1740@naver.com>
2020-04-14 12:01:24 +0900

    add user test

--- api/main/index.js
+++ api/main/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const user = require('../../services/user');
 
 router.get('/', function(req, res, next) {
-    res.render('index');
+    if(req.session.key) {
+        res.render('user/user');
+    } else {
+        res.render('index');
+    }
 });
 router.get('/get', function(req, res, next) {
     res.json(req.session);
