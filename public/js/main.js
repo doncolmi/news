a74bb219e74636316c60b2b209487db118cfd6a2
doncolmi <matathresh1740@naver.com>
2020-04-16 21:24:50 +0900

    add front main

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
 const main = {
     init : function() {
         const _this = this;
-    }
+    },
 };
 
 main.init();
+
+function menu(elem) {
+    const menus = document.getElementsByClassName('elem');
+    for(const item of menus) {
+        item.classList.remove('activeMenu');
+    }
+    elem.classList.add('activeMenu');
+}
